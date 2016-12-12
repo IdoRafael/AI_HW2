c5d34be2f62bf5c50a2fb24f900ef11ca2651f6f
Unguliant <ungulient@gmail.com>
2016-12-12 19:24:35 +0200

    12.12.16 - 19:24

--- cost.py
+++ cost.py
@@ -2,7 +2,11 @@ from ways.graph import current_speed
 
 
 def expected_time(link):
-    if link is None or link.distance == 0:
-        raise ValueError('Link is None or distance == 0')
+    if link is None or current_speed(link) == 0:
+        raise ValueError('Link is None')
     else:
-        return 1000 * current_speed(link) / link.distance
+        current_average_speed = current_speed(link)
+        if current_average_speed == 0:
+            raise ValueError('Current average speed is 0')
+        else:
+            return (link.distance / current_average_speed) / 1000
